8aa1411432a4db391784ab4d5e5ac829ffa9827f
hashburner <killiantayylor@gmail.com>
2024-08-17 21:42:01 +0100

    Sick Update

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#e3ddac',
+        backgroundalt: '#faf4c1',
+        text: '#4c3935',
+        accent: '#ff6666',
+        'accent-dark': '#ff4d4d',
+      },
+    },
+  },
+  plugins: [],
+}
